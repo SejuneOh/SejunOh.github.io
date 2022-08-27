df67b1c0125fa195730790e792046fe72ca064e9
SejuneOh <etry0715@gmail.com>
2022-08-27 17:53:13 +0900

    conf: Tagline text change 'simple is best'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Devlog',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Simple is best',
   url: 'https://SejuneOh.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
